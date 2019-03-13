e87522362446ce07b548e733318412ee59670ec6
khurtado <khurtado@nd.edu>
2019-03-13 02:20:40 +0000

    Execute condor submit as a user.

--- reana_job_controller/condor.py
+++ reana_job_controller/condor.py
@@ -11,11 +11,13 @@
 import json
 import logging
 import os
+import sys
 import time
 import traceback
 import htcondor
 import classad
 from subprocess import check_output
+from retrying import retry
 
 from flask import current_app as app
 from reana_db.database import Session
@@ -23,6 +25,42 @@ from reana_db.models import Job
 
 from reana_job_controller import config, volume_templates
 from reana_job_controller.errors import ComputingBackendSubmissionError
+
+def detach(f):
+    """Decorator for creating a forked process"""
+
+    def fork(*args, **kwargs):
+        pid = os.fork()
+        if pid == 0:
+            try:
+                os.setuid(int(os.environ.get('VC3USERID')))
+                f(*args, **kwargs)
+            finally:
+                os._exit(0)
+
+    return fork
+
+@retry(stop_max_attempt_number=5)
+@detach
+def submit(schedd, sub):
+    try:
+        with schedd.transaction() as txn:
+            clusterid = sub.queue(txn)
+    except Exception as e:
+        logging.debug("Error submission: {0}".format(e))
+        raise(Exception)
+
+    return clusterid
+
+def get_schedd():
+    """Find and return the HTCondor sched.
+    :returns: htcondor schedd object."""
+
+    # Getting remote scheduler
+    schedd_ad = classad.ClassAd()
+    schedd_ad["MyAddress"] = os.environ.get("HTCONDOR_ADDR", None) 
+    schedd = htcondor.Schedd(schedd_ad)
+    return schedd
 
 def condor_instantiate_job(job_id, docker_img, cmd, cvmfs_repos, env_vars, namespace,
                     shared_file_system, job_type):
@@ -47,8 +85,10 @@ def condor_instantiate_job(job_id, docker_img, cmd, cvmfs_repos, env_vars, names
     sub = htcondor.Submit()
     sub['executable'] = '/usr/bin/singularity'
     sub['arguments'] = "exec docker://{0} {1}".format(docker_img,cmd)
-    with schedd.transaction() as txn:
-        clusterid = sub.queue(txn,1)
+    sub['InitialDir'] = '/tmp'
+    clusterid = submit(schedd, sub)
+    # with schedd.transaction() as txn:
+    #     clusterid = sub.queue(txn,1)
 
     return clusterid
 
